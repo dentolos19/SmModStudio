f1221b2a47d0f7e0d037a01e4708df57a0c77eeb
Dennise Catolos <contact@dennise.me>
2020-06-27 15:51:00 +0800

    Created a separate project for backend

---
+++ SmModStudio.Core/Models/SmProject.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace SmModStudio.Core.Models
+{
+    
+    public class SmProject
+    {
+        
+        private static readonly XmlSerializer Serializer = new XmlSerializer(typeof(SmProject));
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        
+        public void Save(string filePath)
+        {
+            var stream = new FileStream(filePath, FileMode.Create);
+            Serializer.Serialize(stream, this);
+            stream.Close();
+        }
+
+        public static SmProject Load(string filePath)
+        {
+            var stream = new FileStream(filePath, FileMode.Open);
+            var result = (SmProject) Serializer.Deserialize(stream);
+            stream.Close();
+            return result;
+        }
+        
+    }
+    
+}
